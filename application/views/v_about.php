79a971ac7527a7c3e119347e395e935d9c6f1573
FatihAkmalH <fatihakmal2424@gmail.com>
2022-04-06 09:05:28 +0700

    menambahkan materi pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Fatih</li>
+        <li>Nama Belakang : Akmal</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Teluk Bone no. 153 Bandar Lampung</li>
+    </ul>
+    <h4>Tempat Lahit</h4>
+    <ul type="none">
+        <li>Bandar Lampung</li>
+    </ul>
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Bola</li>
+    </ul>
+</section>
